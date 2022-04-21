922c2e201fabc29634926f7ccb2501264f27bfd9
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2022-04-21 18:01:58 -0300

    Fix logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import "./App.css";
 import { CatFact } from "components/CatFact";
 import { useState } from "react";
 
-import logo from "./logo.svg";
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo.svg" className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button
