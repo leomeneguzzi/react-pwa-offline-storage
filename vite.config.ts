facf827863d056ed3f97e7c0af322544764a65c5
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2022-04-21 16:56:50 -0300

    Fix PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,42 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({})],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      includeAssets: [
+        "icons/favicon.ico",
+        "icons/apple-touch-icon.png",
+        "icons/apple-touch-icon.png",
+        "robots.txt",
+        "logo.svg",
+      ],
+      manifest: {
+        name: "Cat Fact",
+        short_name: "Cat Fact",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
+  ],
 });
